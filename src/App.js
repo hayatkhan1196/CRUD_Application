40593ff0d0967751e21dc3554501f147100f6fb8
hayatkhan1196 <hayatkhan1196@gmail.com>
2021-04-21 04:34:17 -0700

    crud operation app done

--- src/App.js
+++ src/App.js
@@ -7,18 +7,23 @@ import Contact from "./components/pages/Contact";
 import Navbar from "./components/Layout/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NotFound from "./components/pages/NotFound";
+import Adduser from "./components/user/Adduser";
+import EditUser from "./components/user/EditUser";
+import ViewUser from "./components/user/ViewUser";
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/About' component={About}/>
-          <Route exact path='/Contact' component={Contact}/>
-          <Route component={NotFound}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route exact path="/user/add" component={Adduser} />
+          <Route exact path="/user/edit/:id" component={EditUser} />
+          <Route exact path="/user/:id" component={ViewUser} />
+          <Route component={NotFound} />
         </Switch>
-        
       </div>
     </Router>
   );
